17e07d59583374c25f39bc44cc5ede87bafde550
jeanoza <choikb916@gmail.com>
2022-03-31 17:27:16 +0200

    feat(utils.c): ft_atoi, ft_strlen

---
+++ src/utils.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 16:11:02 by kyubongchoi       #+#    #+#             */
+/*   Updated: 2022/03/31 16:22:01 by kyubongchoi      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+		++i;
+	return (i);
+}
+
+int	ft_is_numeric(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	res;
+	int	sign;
+	int	i;
+
+	i = 0;
+	sign = 1;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		++i;
+	}
+	while (str[i] && ft_is_numeric(str[i]) && i < ft_strlen(str))
+		res = (10 * res) + (str[i++] - '0');
+	return (sign * res);
+}
