2b3a5d770bf6a5c21e8b5f6a9cf0d36aa71b3f13
jeanoza <choikb916@gmail.com>
2022-04-01 09:35:53 +0200

    feat: mutex example

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 09:26:18 by kyubongchoi       #+#    #+#             */
-/*   Updated: 2022/03/31 18:00:05 by kyubongchoi      ###   ########.fr       */
+/*   Updated: 2022/04/01 09:34:33 by kyubongchoi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int	parsing(int ac, char **av)
 // 	return (0);
 // }
 
+//Mutex examples...
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -52,33 +53,40 @@ int	parsing(int ac, char **av)
 #define NUM_THREADS 4
 #endif
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct s_test {
+
+	pthread_mutex_t mutex;
+	int				value;
+}	t_test;
 
 void *func3(void* param)
 {
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&(((t_test*)param)->mutex));
     printf("Incrementing the shared variable...\n");
     for (int i = 0; i < 10000; ++i) {
-        *((int *)param) += 1;
+		((t_test*)param)->value += 1;
     }
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&(((t_test*)param)->mutex));
     return 0;
 }
 
 int main()
 {
     pthread_t threads[NUM_THREADS];
-	int shared = 0;
+	t_test	test;
+	test.value = 0;
+	test.mutex = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
 
 
     for (int i = 0; i < NUM_THREADS; ++i) {
-        pthread_create(&threads[i], NULL, func3, &shared);
+        pthread_create(&threads[i], NULL, func3, &test);
     }
 
     for (int i = 0; i < NUM_THREADS; ++i) {
         pthread_join(threads[i], NULL);
     }
 
-    printf("%d\n", shared);
+    printf("%d\n", test.value);
     exit(EXIT_SUCCESS);
 }
